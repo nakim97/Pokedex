bb7b14e892d13b4683ba5eb011a17bd4b000a724
Stella Kim <62909702+nakim97@users.noreply.github.com>
2021-04-10 17:27:17 -0400

    style pokecard and pokedex

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -7,26 +7,13 @@ const POKE_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprit
 class Pokecard extends Component {
     render() {
         let imgSrc = `${POKE_API}${this.props.id}.png`
-        return ( <
-            div className = "Pokecard" >
-            <
-            h1 > {
-                this.props.name
-            } < /h1> <
-            img src = {
-                imgSrc
-            }
-            alt = {
-                this.props.name
-            }
-            /> <
-            div > Type: {
-                this.props.type
-            } < /div> <
-            div > Exp: {
-                this.props.exp
-            } < /div> <
-            /div>
+        return (
+            <div className = "Pokecard">
+                <h1 className="Pokecard-title"> {this.props.name} </h1>
+                <img src = {imgSrc} alt = {this.props.name}/> 
+                <div className="Pokecard-data"> Type: {this.props.type} </div>
+                <div className="Pokecard-data"> Exp: {this.props.exp} </div> 
+            </div>
         );
     }
 }
